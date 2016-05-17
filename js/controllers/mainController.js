e22d0157824eb18fb409701c4279fab31518d761
nadiafaya <nadiafaya@gmail.com>
2016-05-17 17:24:21 -0300

    Directive to handle post ordering

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,19 +1,29 @@
 app.controller("main", ["$scope", function($scope) {
+	
+	$scope.selectedOrder = { id: "date" };
+
+
 	$scope.posts = [
 		{
 			author: "Nicolás",
-			date: new Date(),
-			content: "Hola carola"
+			date: new Date(2016,5,3),
+			content: "Hola carola",
+			likes: 1,
+			comments: 1
 		},
 		{
 			author: "Nadia",
-			date: new Date(),
-			content: "Vamos boca carajo"
+			date: new Date(2016,5,2),
+			content: "Vamos boca carajo",
+			likes: 3,
+			comments: 5
 		},
 		{
 			author: "Nicolás",
-			date: new Date(),
-			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac cursus turpis, at dictum lorem. Aliquam feugiat massa quis tortor venenatis, tristique vehicula mauris mollis. Vestibulum elementum ipsum quis augue mollis, dictum consectetur felis blandit. Cras venenatis euismod nibh at vestibulum. Fusce ante metus, rutrum vel odio in, mattis dapibus nisl. Vestibulum et placerat nibh, at sollicitudin risus. Donec ligula leo, commodo vitae arcu non, eleifend condimentum dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur maximus ex augue, sit amet blandit tellus iaculis a."
+			date: new Date(2016,5,1),
+			content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac cursus turpis, at dictum lorem. Aliquam feugiat massa quis tortor venenatis, tristique vehicula mauris mollis. Vestibulum elementum ipsum quis augue mollis, dictum consectetur felis blandit. Cras venenatis euismod nibh at vestibulum. Fusce ante metus, rutrum vel odio in, mattis dapibus nisl. Vestibulum et placerat nibh, at sollicitudin risus. Donec ligula leo, commodo vitae arcu non, eleifend condimentum dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur maximus ex augue, sit amet blandit tellus iaculis a.",
+			likes: 7,
+			comments: 2
 		}
 	];
 }]);
---
+++ js/directives/postListMenuItem.js
@@ -0,0 +1,23 @@
+app.directive("postListMenuItem", [function() {
+	return {
+		restrict: "C",
+		transclude: true,
+		replace: true,
+		scope: true, // create a new scope
+		templateUrl: "/templates/postListMenuItem.html",
+		link: function(scope, elem, attrs) {
+			scope.orderBy = attrs.orderBy;
+			scope.isSelected = function() {
+				return scope.selectedOrder.id === scope.orderBy;
+			};
+		},
+		controller: function($scope) {
+			$scope.reOrderPosts = function() {
+				$scope.selectedOrder.id = $scope.orderBy;
+				$scope.posts = $scope.posts.sort(function(a, b){
+					return a[$scope.orderBy] < b[$scope.orderBy];
+				});
+			};
+		}
+	};
+}])
