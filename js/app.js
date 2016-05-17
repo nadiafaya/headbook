1bc9fb2f0038eeadd774cf32b9e829fb375b9680
nadiafaya <nadiafaya@gmail.com>
2016-05-17 17:46:22 -0300

    Added basic routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-var app = angular.module("app", []);
+var app = angular.module("app", ['ngRoute']);
+
+app.config(['$routeProvider',
+  	function($routeProvider) {
+    	$routeProvider.
+      	when('/', {
+        	templateUrl: 'views/home.html',
+        	controller: 'main'
+      	}).
+      	otherwise({
+        	redirectTo: '/'
+      	});
+	}
+]);
